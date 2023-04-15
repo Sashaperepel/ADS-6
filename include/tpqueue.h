9cb163b108fc387b9b298de9fc6a700c0402e96b
Sashaperepel <124892766+Sashaperepel@users.noreply.github.com>
2023-04-15 14:23:01 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -11,40 +11,40 @@ class TPQueue {
  public:
    TPQueue() : first(0), last(0), count(0) {
    arr = new T[size];
-	 }
-	 ~TPQueue() {
-	   delete[] arr;
-	 }
-	 int isEmpty() const {
-		 return count == 0;
-	 }
-	 int isFull() const {
-		 return count == size;
-	 }
-	 void push(const T& value) {
-		 if (isFull()) {
-		   throw std::string("Full");
-		 } else {
+   }
+   ~TPQueue() {
+     delete[] arr;
+   }
+   int isEmpty() const {
+     return count == 0;
+   }
+   int isFull() const {
+     return count == size;
+   }
+   void push(const T& value) {
+     if (isFull()) {
+       throw std::string("Full");
+     } else {
          int temp = last;
-			   for (int i = last; i > first; i--) {
-				   if (arr[i - 1].prior < value.prior) {
-				     temp = i - 1;
-				     arr[i % size] = arr[i - 1];
-				   }
-			   }
-			   arr[temp % size] = value;
-			   count++;
-			   last++;
-		 }
+	 for (int i = last; i > first; i--) {
+	   if (arr[i - 1].prior < value.prior) {
+	     temp = i - 1;
+	     arr[i % size] = arr[i - 1];
+	   }
 	 }
-	 const T& pop() {
-		 if (isEmpty()) {
-		   throw std::string("Empty");
-		 } else {
-			   count--;
-			   return arr[first++ % size];
-		 }
-	 }
+	 arr[temp % size] = value;
+	 count++;
+	 last++;
+    }
+  }
+  const T& pop() {
+    if (isEmpty()) {
+      throw std::string("Empty");
+    } else {
+        count--;
+	return arr[first++ % size];
+    }
+  }
 };
 
 struct SYM {
